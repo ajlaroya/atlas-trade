36fb541f634f9f3e0d83d4034e4b2f53d3ab0d0f
Arthur Laroya <arthur.j.laroya@gmail.com>
2025-11-08 15:24:43 +1100

    implement home page with reusable TradingView widgets, tradingview hooks, layout, header, nav, userdropdown, avatar, new favicon and logo

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark scroll-smooth">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="min-h-screen text-gray-400">
+          <Header />
+          <div className="container py-10">{children}</div>
+        </main>
       </body>
     </html>
   );
